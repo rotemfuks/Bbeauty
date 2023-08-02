afea44419510db9fb3d98db34e32c9e04be97a64
rotemfuks <bar74064@gmail.com>
2023-08-02 21:53:15 +0300

    updated about contect. design for about,home and app width

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import styles from "./About.module.scss";
 export function About() {
   return (
     <div>
-      <h1 className={styles.aboutTitle}>Bbeauty</h1>
+      <h2 className={styles.aboutTitle}>Bbeauty</h2>
 
       <div className={styles.container}>
         <div className={styles.paragraph}>
@@ -15,16 +15,39 @@ export function About() {
           professionalism.
         </div>
         <div className={styles.paragraph}>
-          Embrace the art of making a lasting impression with business cards
-          that beautifully blend style and functionality. Each card is
-          meticulously crafted to showcase your brand's essence, while providing
-          essential contact information that opens doors to new opportunities.
-          With a seamless browsing experience, you can effortlessly explore our
-          selection of modern and chic designs, filtering by your preferences to
-          find the perfect match. Personalize your chosen card with your brand's
-          logo, colors, and details to create a powerful visual representation
-          of your cosmetics business.
+          Using Our Website: We've built our website using the latest
+          technologies, combining React, TypeScript, and React Bootstrap to
+          offer a seamless user experience. Here's a guide on how to use our
+          website to create your perfect beauty business card:
         </div>
+        <div className={styles.paragraph}>
+          1. We offer to register / Log In: To get started, register for an
+          account on our platform or log in if you already have one. This will
+          give you access to all the design features and templates.
+        </div>
+        <div className={styles.paragraph}>
+          2. Browse through our diverse collection of beauty-themed business
+          card templates. Select the one that best aligns with your demands.
+        </div>
+        <div className={styles.paragraph}>
+          3.You can contact the business or just look and get inspired.
+        </div>
+
+        <div className={styles.paragraph}>
+          4. If you are a business owner you can add your business to the system
+          so other users can get inspired to go and reach your business.
+        </div>
+        <div className={styles.paragraph}>
+          5. When you get to the home page you can make changes in your cards,
+          add to your favorites, delete and more all depends on your
+          authorization as registered user.
+        </div>
+        <div className={styles.paragraph}>
+          6. If you are intrested you can click on the sun icon on the top right
+          and change the app to be on dark mode.
+        </div>
+
+        {/* 
         <div className={styles.paragraph}>
           We understand that every detail matters in the world of beauty and
           aesthetics, and our commitment to quality extends to printing options.
@@ -39,7 +62,7 @@ export function About() {
         <div className={styles.paragraph}>
           It's time to make your mark with cards that leave a lasting impression
           and open doors to endless possibilities in the world of beauty.
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,11 @@
-import { FunctionComponent, useContext, useEffect } from "react";
+import { FunctionComponent, useContext } from "react";
 import BusinessCards from "./BusinessCards";
 import { Button } from "react-bootstrap";
 import { BsBuildingAdd } from "react-icons/bs";
 import { LoginContext } from "../context/LoginContext";
 import { useNavigate } from "react-router-dom";
 import { CardContext } from "../context/CardContext";
+import styles from "./Home.module.scss";
 interface HomeProps {}
 
 const Home: FunctionComponent<HomeProps> = () => {
@@ -18,8 +19,8 @@ const Home: FunctionComponent<HomeProps> = () => {
 
   return (
     <>
-      <h1> Home Page</h1>
-      <div>
+      <h2 className={styles.title}> Bbeauty</h2>
+      <div className={styles.paragraph}>
         Welcome to our cosmetics business card showcase! Step into a world of
         stunning and captivating designs tailored exclusively for the cosmetics
         industry. Whether you're a makeup artist, hairstylist, beauty salon
