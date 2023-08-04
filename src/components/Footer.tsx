001ff3d2ce38d8c26a937cb6837e4217c7bb9f57
rotemfuks <bar74064@gmail.com>
2023-08-04 17:41:03 +0300

    footer changes and height change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,13 @@
 import { FunctionComponent, useContext } from "react";
 import styles from "./Footer.module.scss";
+
 import { LoginContext } from "../context/LoginContext";
 import {
   BsFillFunnelFill,
   BsFillHeartFill,
   BsCardHeading,
 } from "react-icons/bs";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 interface FooterProps {}
 
@@ -21,16 +22,16 @@ const Footer: FunctionComponent<FooterProps> = () => {
   };
   return (
     <footer className={styles.footer}>
-      <Container>
-        <Row>
-          <Col lg={3} md={6} sm={12}>
+      <div className={styles.containerfooter}>
+        <div className={styles.rowfooter}>
+          <div className={styles.footerCol}>
             <div className={styles.footerColumn}>
               <h5>Website made to promot your beauty business.</h5>
               {/* Add your website details and credits here */}
               <p>©Rotem {new Date().getFullYear()} Bbeauty</p>
             </div>
-          </Col>
-          <Col lg={2} md={6} sm={6}>
+          </div>
+          <div className={styles.footerCol}>
             <div className={styles.footerColumn}>
               <Link to={"about"}>
                 <h5>
@@ -40,9 +41,9 @@ const Footer: FunctionComponent<FooterProps> = () => {
               </Link>
               {/* Add your about content here */}
             </div>
-          </Col>
+          </div>
           {user?.isBusiness && (
-            <Col lg={2} md={6} sm={6}>
+            <div className={styles.footerCol}>
               <div className={styles.footerColumn}>
                 <Link to={"my-cards"}>
                   <h5>
@@ -52,10 +53,10 @@ const Footer: FunctionComponent<FooterProps> = () => {
                 </Link>
                 {/* Add your my cards content here */}
               </div>
-            </Col>
+            </div>
           )}
           {user && (
-            <Col lg={2} md={6} sm={6}>
+            <div className={styles.footerCol}>
               <div className={styles.footerColumn}>
                 <Link to={"favorites"}>
                   <h5>
@@ -64,19 +65,75 @@ const Footer: FunctionComponent<FooterProps> = () => {
                 </Link>
                 {/* Add your favorites content here */}
               </div>
-            </Col>
+            </div>
           )}
-
-          {user && (
-            <Col lg={3} md={6} sm={6}>
+          <div className={styles.footerCol}>
+            {user && (
               <Button className={styles.logoutInButton} onClick={onLogoutClick}>
                 Log Out
               </Button>
-            </Col>
-          )}
-        </Row>
-      </Container>
+            )}
+          </div>
+        </div>
+      </div>
     </footer>
+    // <footer className={styles.footer}>
+    //   <Container>
+    //     <Row>
+    //       <Col lg={3} md={6} sm={12}>
+    //         <div className={styles.footerColumn}>
+    //           <h5>Website made to promot your beauty business.</h5>
+    //           {/* Add your website details and credits here */}
+    //           <p>©Rotem {new Date().getFullYear()} Bbeauty</p>
+    //         </div>
+    //       </Col>
+    //       <Col lg={2} md={6} sm={6}>
+    //         <div className={styles.footerColumn}>
+    //           <Link to={"about"}>
+    //             <h5>
+    //               <BsFillFunnelFill />
+    //               About
+    //             </h5>
+    //           </Link>
+    //           {/* Add your about content here */}
+    //         </div>
+    //       </Col>
+    //       {user?.isBusiness && (
+    //
+    //           <div className={styles.footerColumn}>
+    //             <Link to={"my-cards"}>
+    //               <h5>
+    //                 <BsCardHeading />
+    //                 My Cards
+    //               </h5>
+    //             </Link>
+    //             {/* Add your my cards content here */}
+    //           </div>
+    //
+    //       )}
+    //       {user && (
+    //
+    //           <div className={styles.footerColumn}>
+    //             <Link to={"favorites"}>
+    //               <h5>
+    //                 <BsFillHeartFill /> Favorites
+    //               </h5>
+    //             </Link>
+    //             {/* Add your favorites content here */}
+    //           </div>
+    //
+    //       )}
+
+    //       {user && (
+    //         <Col lg={3} md={6} sm={6}>
+    //           <Button className={styles.logoutInButton} onClick={onLogoutClick}>
+    //             Log Out
+    //           </Button>
+    //         </Col>
+    //       )}
+    //     </Row>
+    //   </Container>
+    // </footer>
   );
 };
 
